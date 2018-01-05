5058dc1cb685a8f789c9633bdc31a33a4231f423
Jean Doyon <jdoyon@gmail.com>
2018-01-05 11:58:47 -0500

    Update mddelcc_ri_parser.py

--- python/mddelcc_ri_parser.py
+++ python/mddelcc_ri_parser.py
@@ -4,6 +4,11 @@ import codecs
 import time
 import os
 
+def log(program, entry):
+    program = program
+    entry = entry
+    command = "python3 logger.py -p %s -e %s" % (program, entry)
+    return command
 
 def html_parser():
 
@@ -15,18 +20,23 @@ def html_parser():
             url = r'https://geoindex.xyz/files/mddelcc_ri_region' + str(number) + '.html'
 
         # Build HTML file
+        os.system(log("\"MDDELCC_PARSER_RI.PY:HTML_PARSER\"", "\"Pulling from " + url + "\""))
         html = requests.get(url).content
         # Make our soup
+        os.system(log("\"MDDELCC_PARSER_RI.PY:HTML_PARSER\"", "\"Parsing big html\""))
         soup = BeautifulSoup(html, 'lxml')
 
         date_string = time.strftime("%Y%m%d")
+        os.system(log("\"MDDELCC_PARSER_RI.PY:HTML_PARSER\"", "\"Creating file " + 'mddelcc_ri_in_' + date_string + '.txt' + "\""))
         f = codecs.open('mddelcc_ri_in_' + date_string + '.txt', 'a+', encoding='utf-8')
 
 
         # Remove comments from file
+        os.system(log("\"MDDELCC_PARSER_RI.PY:HTML_PARSER\"", "\"Removing comments from html file\""))
         comments = soup.findAll(text=lambda text: isinstance(text, Comment))
         [comment.extract() for comment in comments]
 
+        os.system(log("\"MDDELCC_PARSER_RI.PY:HTML_PARSER\"", "\"Iterating on tr tags - extracting td tags\""))
         # Start at 5 in order not to pull table headers. Stop at len -2 so we do not pick up the table footer
         for i in range(5, (len(soup('tr')) - 2)):
             td_list = soup('tr')[i]
@@ -38,9 +48,8 @@ def html_parser():
                 # print(td.text)
                 f.write(tdstr + '|')
             f.writelines("\n")
-
-
         f.close()
+        os.system(log("\"MDDELCC_PARSER_RI.PY:HTML_PARSER\"", "\"Cleaning file " + 'mddelcc_ri_in_' + date_string + '.txt' + "\""))
     clean_file()
 
 
@@ -48,12 +57,16 @@ def clean_file():
 
     date_string = time.strftime("%Y%m%d")
     f_in = codecs.open('mddelcc_ri_in_' + date_string + '.txt', 'rb', encoding='utf-8')
+    os.system(log("\"MDDELCC_PARSER_RI.PY:HTML_PARSER:CLEAN_FILE\"", "\"Writing file " + 'mddelcc_ri_in_' + date_string + '.csv' + "\""))
     f_out = codecs.open('MDDELCC_RI_' + date_string + '.csv', 'w', encoding='utf-8')
 
     # Remove white spaces and write to output file
     for line in f_in:
         f_out.write(" ".join(line.split()) + '\n')
     f_in.close()
+    os.system(log("\"MDDELCC_PARSER_RI.PY:HTML_PARSER:CLEAN_FILE\"", "\"Deleting file " + 'mddelcc_ri_in_' + date_string + '.txt' + "\""))
     os.remove('mddelcc_ri_in_' + date_string + '.txt')
+
+
 
 html_parser()
